a218d504cc52f28c1b6bd17beea9885c42774bfd
lbferrand <lbferrand@gmail.com>
2022-05-19 21:02:34 -0400

    Add header and About component

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
     AboutUs
   </div>
 );
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 
+import { SubHeading } from "../../components";
 import { images } from "../../constants";
 import "./Header.css";
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
-    <div className="app__wrapper_info"></div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        In our breathtaking dining room, fantastic views and savor culinary
+        masterpieces set the scene for an unforgettable experience. Our
+        world-renowned chef sources seafood, produce and spices from the islands
+        to create a menu worthy of paradise. The choice of location is yours, as
+        intimate indoor and outdoor seating is available along with dreamy
+        beachside tables. For those desiring a private experience, our
+        award-winning service redefines in-room dining.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
 
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img"></img>
+    </div>
   </div>
 );
 
