86af49b08ee124a4a0d2b23d76623c55b40e586f
lbferrand <lbferrand@gmail.com>
2022-05-26 22:51:30 -0400

    Create footer, footerOverlay and Newsletter components

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './FooterOverlay.css';
+import "./FooterOverlay.css";
 
 const FooterOverlay = () => (
-  <div>
+  <div className="app__footerOverlay">
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
     FooterOverlay
   </div>
 );
